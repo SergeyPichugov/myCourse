141c6b3efd6f566ef05ab0f39a904baecd7218b3
Sergey Pichugov <polarizaciacd@gmail.com>
2021-01-29 14:51:00 +0300

    feat(script.js)lesson02 complete

--- script/script.js
+++ script/script.js
@@ -1,11 +1,21 @@
 
-let money,
-income,
-addExpenses,
-deposit,
-mission,
-period;
+const money = 150000;
+const income = '49550';
+const addExpenses = 'проезд, еда, интернет';
+const deposit = true;
+const mission = 94513548;
+const period = 8;
 
-alert ( 'Привет!' );
 
-console.log('Дорога возникает под ногами идущего.');
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' рублей');
+console.log(addExpenses.toLowerCase().split(' ,'));
+
+const budgetDay = money / 30;
+console.log('budgetDay: ', budgetDay);
+
+
