f03464f4e629235238bb0e7e1a061363b5cc2bdf
Sergey Pichugov <polarizaciacd@gmail.com>
2021-02-01 11:13:25 +0300

    fix(script.js): minor fixes

--- script/script.js
+++ script/script.js
@@ -1,21 +1,19 @@
 
-const money = 150000;
-const income = '49550';
-const addExpenses = 'проезд, еда, интернет';
-const deposit = true;
-const mission = 94513548;
-const period = 8;
+const money = 150000,
+   income = '49550',
+   addExpenses = 'проезд, еда, интернет',
+   deposit = true,
+   mission = 94513548,
+   period = 8,
+   budgetDay = money / 30;
 
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
+console.log(typeof money, typeof income, typeof deposit);
 console.log(addExpenses.length);
 console.log('Период равен ' + period + ' месяцев');
 console.log('Цель заработать ' + mission + ' рублей');
 console.log(addExpenses.toLowerCase().split(' ,'));
 
-const budgetDay = money / 30;
 console.log('budgetDay: ', budgetDay);
 
 
